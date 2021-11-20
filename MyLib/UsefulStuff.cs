2f449ff70d446129b205265cc83638d535494856
Eduardo Hinojosa (Frost) <30810344+FS-Frost@users.noreply.github.com>
2021-11-19 21:28:49 -0300

    Initial commit.

---
+++ MyLib/UsefulStuff.cs
@@ -0,0 +1,19 @@
+﻿namespace MyLib {
+    public class Person {
+        /// <summary>
+        /// The name.
+        /// </summary>
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString() {
+            return $"Person{{Name: {Name}, Age: {Age}}}";
+        }
+    }
+
+    public class UsefulStuff {
+        public static Person NewPerson() {
+            return new Person { Age = 27, Name = "Frost" };
+        }
+    }
+}
